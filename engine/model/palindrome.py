2394aa2d6f6ba194051cf115d1d9719bd5c94102
7bernie <bernardbassie@gmail.com>
2023-09-08 22:47:25 +0100

    My refactored code

--- engine/model/palindrome.py
+++ engine/model/palindrome.py
@@ -1,12 +1,9 @@
-from datetime import datetime
 
+m datetime import datetime
 from engine.sternman_engine import SternmanEngine
 
-
 class Palindrome(SternmanEngine):
-    def needs_service(self):
-        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
-        if service_threshold_date < datetime.today().date() or self.engine_should_be_serviced():
-            return True
-        else:
-            return False
+        def needs_service(self) -> bool:
+                    service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+                            return service_threshold_date < datetime.today().date() or self.engine_should_be_serviced()
+
